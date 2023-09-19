a727032a95f9b8472630712a7bfbb4d2eac1ba99
anwar <anwarhk.ah@gmail.com>
2023-09-19 17:53:06 +0530

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,12 @@
     <title>Product</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 <body>
-    <div id="app"> <!-- Add this Vue container -->
+    <div id="app">
         @yield('content')
     </div> 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,18 @@
 
 @section('content')
 <div class="container">
-    <h1>Welcome to the Laravel Application</h1>
-    <div class="mt-4">
-        <!-- Button 1: Product CRUD with Action -->
-        <a href="{{ route('products.index') }}" class="btn btn-primary">Product CRUD with Action</a>
+    <div class="mt-4 text-center">
+        <a href="{{ route('products.index') }}" class="btn btn-primary">
+            <i class="fas fa-cogs"></i> Product CRUD with Action
+        </a>
         
-        <!-- Button 2: Product CRUD with Ajax -->
-        <a href="{{ route('products-ajax-action.index') }}" class="btn btn-success">Product CRUD with Ajax</a>
+        <a href="{{ route('products-ajax-action.index') }}" class="btn btn-success">
+            <i class="fas fa-database"></i> Product CRUD with Ajax
+        </a>
         
-        <!-- Button 3: Dialer -->
-        <a href="{{ route('dailer') }}" class="btn btn-warning">Dialer</a>
+        <a href="{{ route('dailer') }}" class="btn btn-warning">
+            <i class="fas fa-phone"></i> Dialer
+        </a>
     </div>
 </div>
 @endsection
